d4ff491084337c28e58b12cb2a33b6243df56874
Sohex <616447sohanghosh@gmail.com>
2026-06-27 14:01:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.58%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def checkPerfectNumber(self, num):
-        if num < 2:
-            return False
-        i = 2
-        s = 1
-        while i*i <= num:
-            if num % i == 0:
-                s+= i
-                if i*i != num:
-                    s+=num//i
-            i+=1
-        return s == num
+        return num in {6, 28, 496, 8128, 33550336}
+        # if num < 2:
+        #     return False
+        # i = 2
+        # s = 1
+        # while i*i <= num:
+        #     if num % i == 0:
+        #         s+= i
+        #         if i*i != num:
+        #             s+=num//i
+        #     i+=1
+        # return s == num
 
 
         """
