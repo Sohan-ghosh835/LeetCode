f20b80e0ed0e17b9955a38a536c5cd0009c0bb7e
Sohex <616447sohanghosh@gmail.com>
2026-06-28 14:56:09 +0530

    Time: 17 ms (67.15%), Space: 25.9 MB (56.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        s = set()
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
+        s = set(nums)
+        if len(s)!=len(nums):
+            return True
         return False
+        # s = set()
+        # for num in nums:
+        #     if num in s:
+        #         return True
+        #     s.add(num)
+        # return False
         """
         :type nums: List[int]
         :rtype: bool
