61ccde73416a6cb987297d69fec3db4164d70bda
Sohex <616447sohanghosh@gmail.com>
2026-06-29 20:38:39 +0530

    Time: 0 ms (100%), Space: 12.7 MB (18.17%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,18 +1,23 @@
 class Solution(object):
     def judgeCircle(self, moves):
-        a = [0,0]
-        for ch in moves:
-            if ch == "U":
-                a[0] += 1
-            elif ch == "D":
-                a[0] -= 1
-            elif ch == "L":
-                a[1] += 1
-            elif ch == "R":
-                a[1] -= 1
-            else:
-                continue
-        return a == [0,0]
+        lr= moves.count("R") - moves.count("L")
+        ud= moves.count("U") - moves.count("D")
+        if lr==0 and ud==0:
+            return True
+        return False
+        # a = [0,0]
+        # for ch in moves:
+        #     if ch == "U":
+        #         a[0] += 1
+        #     elif ch == "D":
+        #         a[0] -= 1
+        #     elif ch == "L":
+        #         a[1] += 1
+        #     elif ch == "R":
+        #         a[1] -= 1
+        #     else:
+        #         continue
+        # return a == [0,0]
         """
         :type moves: str
         :rtype: bool
