154661c5de4aa8193ec4b7b0c551e2844fa29342
Sohex <616447sohanghosh@gmail.com>
2026-06-30 21:54:25 +0530

    Time: 3 ms (69.62%), Space: 14.7 MB (17.58%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def singleNumber(self, nums):
-        a = []
-        s = Counter(nums)
-        for i in nums:
-            if s[i] == 1:
-                a.append(i)
-        return a
+        freq = defaultdict(int)  
+        for n in nums:
+            freq[n] += 1
+        res = []
+        for num, f in freq.items():
+            if f == 1:
+                res.append(num)
+        return res
+        # a = []
+        # s = Counter(nums)
+        # for i in nums:
+        #     if s[i] == 1:
+        #         a.append(i)
+        # return a
         """
         :type nums: List[int]
         :rtype: List[int]
