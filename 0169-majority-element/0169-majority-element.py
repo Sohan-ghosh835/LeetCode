c5c10c4af5bf3fedd5c67182f050561e3ac872c8
Sohex <616447sohanghosh@gmail.com>
2026-09-12 02:49:27 +0530

    Time: 19 ms (12.87%), Space: 13.7 MB (40.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,6 @@
+from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
-        cand = nums[0]
-        count = 0
-        for n in nums:
-            if count == 0:
-                cand = n
-            count += 1 if cand == n else -1
-        return cand 
-                
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        c = Counter(nums)
+        return c.most_common(1)[0][0]
         
